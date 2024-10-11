d0cd9e233bd4d689c09d00876ce6374435ec8621
ESCORPIO <jose.38.rf@gmail.com>
2024-10-11 12:01:55 -0500

    tienda virtual

---
+++ Controllers/Clientes.php
@@ -0,0 +1,136 @@
+<?php 
+
+	class Clientes extends Controllers{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function Clientes()
+		{
+			$data['page_tag'] = "Clientes";
+			$data['page_title'] = "CLIENTES <small>Tienda Virtual</small>";
+			$data['page_name'] = "clientes";
+			$data['page_functions_js'] = "functions_clientes.js";
+			$this->views->getView($this,"clientes",$data);
+		}
+
+		public function setCliente(){
+			if($_POST){
+				
+				if(empty($_POST['txtIdentificacion']) || empty($_POST['txtNombre']) || empty($_POST['txtApellido']) || empty($_POST['txtTelefono']) || empty($_POST['txtEmail']) || empty($_POST['listRolid']) || empty($_POST['listStatus']) )
+				{
+					$arrResponse = array("status" => false, "msg" => 'Datos incorrectos.');
+				}else{ 
+					$idUsuario = intval($_POST['idUsuario']);
+					$strIdentificacion = strClean($_POST['txtIdentificacion']);
+					$strNombre = ucwords(strClean($_POST['txtNombre']));
+					$strApellido = ucwords(strClean($_POST['txtApellido']));
+					$intTelefono = intval(strClean($_POST['txtTelefono']));
+					$strEmail = strtolower(strClean($_POST['txtEmail']));
+					$intTipoId = intval(strClean($_POST['listRolid']));
+					$intStatus = intval(strClean($_POST['listStatus']));
+
+					if($idUsuario == 0)
+					{
+						$option = 1;
+						$strPassword =  empty($_POST['txtPassword']) ? hash("SHA256",passGenerator()) : hash("SHA256",$_POST['txtPassword']);
+						$request_user = $this->model->insertCliente($strIdentificacion,
+																$strNombre, 
+																$strApellido,
+																$intTelefono, 
+																$strEmail,
+																$strPassword, 
+																$intTipoId, 
+																$intStatus );
+					}else{
+						$option = 2;
+						$strPassword =  empty($_POST['txtPassword']) ? "" : hash("SHA256",$_POST['txtPassword']);
+						$request_user = $this->model->updateCliente($idUsuario,
+														$strIdentificacion, 
+														$strNombre,
+														$strApellido,
+														$intTelefono, 
+														$strEmail,
+														$strPassword, 
+														$intTipoId, 
+														$intStatus);
+
+					}
+
+					if($request_user > 0 )
+					{
+						if($option == 1){
+							$arrResponse = array('status' => true, 'msg' => 'Datos guardados correctamente.');
+						}else{
+							$arrResponse = array('status' => true, 'msg' => 'Datos Actualizados correctamente.');
+						}
+					}else if($request_user == 'exist'){
+						$arrResponse = array('status' => false, 'msg' => '¡Atención! el email o la identificación ya existe, ingrese otro.');		
+					}else{
+						$arrResponse = array("status" => false, "msg" => 'No es posible almacenar los datos.');
+					}
+				}
+				echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+			}
+			die();
+		}
+
+		public function getClientes()
+		{
+			$arrData = $this->model->selectClientes();
+			for ($i=0; $i < count($arrData); $i++) {
+
+				if($arrData[$i]['status'] == 1)
+				{
+					$arrData[$i]['status'] = '<span class="badge badge-success">Activo</span>';
+				}else{
+					$arrData[$i]['status'] = '<span class="badge badge-danger">Inactivo</span>';
+				}
+
+				$arrData[$i]['options'] = '<div class="text-center">
+				<button class="btn btn-info btn-sm btnViewUsuario" onClick="fntViewCliente('.$arrData[$i]['idpersona'].')" title="Ver Cliente"><i class="far fa-eye"></i></button>
+				<button class="btn btn-primary  btn-sm btnEditUsuario" onClick="fntEditCliente('.$arrData[$i]['idpersona'].')" title="Editar Cliente"><i class="fas fa-pencil-alt"></i></button>
+				<button class="btn btn-danger btn-sm btnDelUsuario" onClick="fntDelCliente('.$arrData[$i]['idpersona'].')" title="Eliminar Cliente"><i class="far fa-trash-alt"></i></button>
+				</div>';
+			}
+			echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
+			die();
+		}
+
+		public function getCliente(int $idpersona){
+			
+			$idusuario = intval($idpersona);
+			if($idusuario > 0)
+			{
+				$arrData = $this->model->selectCliente($idusuario);
+				if(empty($arrData))
+				{
+					$arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
+				}else{
+					$arrResponse = array('status' => true, 'data' => $arrData);
+				}
+				echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+			}
+			die();
+		}
+
+
+		public function delCliente()
+		{
+			if($_POST){
+				$intIdpersona = intval($_POST['idUsuario']);
+				$requestDelete = $this->model->deleteCliente($intIdpersona);
+				if($requestDelete)
+				{
+					$arrResponse = array('status' => true, 'msg' => 'Se ha eliminado el usuario');
+				}else{
+					$arrResponse = array('status' => false, 'msg' => 'Error al eliminar el usuario.');
+				}
+				echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+			}
+			die();
+		}
+
+	}
+ ?>
